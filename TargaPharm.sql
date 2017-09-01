ccf67d749e7f1766161be3997c9a46d707aa3d93
revamped83 <4150015@student.swin.edu.au>
2017-09-01 17:29:26 +1000

    Created new SQL database to match ERD-Diagram-v2.png

--- TargaPharm.sql
+++ TargaPharm.sql
@@ -7,64 +7,77 @@ USE TargaPharm;
 -- --------------------------------------------------------
 
 --
--- Table structure for table 'item'
---
+-- Table structure for table 'Item'
+-- Represents all items in inventory
 
 CREATE TABLE Item (
-  ItID          int         NOT NULL AUTO_INCREMENT,
+  ItemID          int         NOT NULL AUTO_INCREMENT,
   ItName        varchar(50) DEFAULT NULL,
   ItCat         varchar(20) DEFAULT NULL,
   ItStockLevel  int         DEFAULT NULL,
-  PRIMARY KEY     (ItID)
+  PRIMARY KEY     (ItemID)
 ); 
 
 --
--- Table structure for table 'restock'
---
+-- Table structure for table 'ItemPurchase'
+-- Represents purchases from the wholesaler to restock inventory
 
-CREATE TABLE StockPurchase (
-  ItID            int         NOT NULL,
+CREATE TABLE ItemPurchase (
+  ItemID            int         NOT NULL,
   TimeDate        datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  ItQuantity      int         DEFAULT NULL,
-  WholesalePrice  Decimal     DEFAULT NULL,
-  PRIMARY KEY (ItID, TimeDate),
-  FOREIGN KEY (ItID) REFERENCES Item (ItID)
+  ItQuantity      int         NOT NULL,
+  WholesalePrice  Decimal     NOT NULL,
+  PRIMARY KEY (ItemID, TimeDate),
+  FOREIGN KEY (ItemID) REFERENCES Item (ItemID)
 ); 
 
 --
--- Table structure for table 'sale'
---
+-- Table structure for table 'CustomerSale'
+-- Keeps a record of each customer purchase, its date/time and receipt subtotal
 
 CREATE TABLE CustomerSale (
-  ItID          int           NOT NULL,
+  SaleID        int           NOT NULL,
   TimeDate      datetime      NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  ItQuantity    int           DEFAULT NULL,
-  RetailPrice   Decimal       DEFAULT NULL,
-  PRIMARY KEY (ItID, TimeDate),
-  FOREIGN KEY (ItID) REFERENCES Item (ItID)
+  SubTotal      Decimal       NOT NULL,
+  PRIMARY KEY (SaleID)
   );
 
+
+--
+-- Table structure for table 'ItemSale'
+-- Keeps a record of all the items in each sale to customer, their quantities and the retail price per unit
+
+CREATE TABLE ItemSale (
+  SaleID        int           NOT NULL,
+  ItemID        int           NOT NULL,
+  UnitPrice     Decimal       NOT NULL,
+  Quantity      int           DEFAULT NULL,
+  PRIMARY KEY (ItemID, SaleID),
+  FOREIGN KEY (SaleID) REFERENCES CustomerSale (SaleID),
+  FOREIGN KEY (ItemID) REFERENCES Item (ItemID)
+  );
 
 --
 -- Dumping data for table 'item'
 --
 
-INSERT INTO Item (ItID, ItName, ItCat, ItStockLevel) VALUES
+INSERT INTO Item (ItemID, ItName, ItCat, ItStockLevel) VALUES
 (1, 'Colgate Minty Toothpaste', 'Dental Hygiene', 20),
 (2, 'Colgate Dental Tape', 'Dental Hygiene', 30),
 (3, 'Band-aids - Standard', 'Wound Dressings', 20),
 (4, 'Panadeine Capsules - 30 Box', 'Analgesic', 20),
 (5, 'Rexona Sport - Women - Roll-on', 'Personal Hygiene', 20),
-(6, 'Aspro-Clear Effervescent Tablets - 30 Box', 'Analgesic', 20);
+(6, 'Aspro-Clear Effervescent Tablets - 30 Box', 'Analgesic', 20),
+(7, 'Fishermans Friend Cough Drops', 'Throat Lozenge', 20);
 
 -- --------------------------------------------------------
 
 
 --
--- Insert data for table 'StockPurchase'
+-- Insert data for table 'ItemPurchase'
 --
 
-INSERT INTO StockPurchase (ItID, ItQuantity, WholesalePrice) VALUES
+INSERT INTO ItemPurchase (ItemID, ItQuantity, WholesalePrice) VALUES
 (1, 20, 1.50),
 (2, 20, 1.12),
 (3, 20, 3.50),
@@ -76,14 +89,27 @@ INSERT INTO StockPurchase (ItID, ItQuantity, WholesalePrice) VALUES
 
 
 --
--- Dumping data for table 'sale'
+-- Dumping data for table 'CustomerSale'
 --
 
-INSERT INTO CustomerSale (ItID, ItQuantity, RetailPrice) VALUES
-(1, 1, 3),
-(2, 1, 2.24),
-(3, 2, 7),
-(4, 1, 4.48),
-(5, 1, 5.6),
-(6, 2, 4.88);
+INSERT INTO CustomerSale (SaleID, SubTotal) VALUES
+(1, 12.50),
+(2, 5.00),
+(3, 7.00),
+(4, 2.95),
+(5, 12.50),
+(6, 45.00);
 
+--
+-- Dumping data for table 'ItemSale'
+--
+
+INSERT INTO ItemSale (SaleID, ItemID, UnitPrice, Quantity) VALUES
+(1, 1, 4.50, 2),
+(1, 2, 3.50, 1),
+(2, 5, 5.00, 1),
+(3, 3, 3.50, 2),
+(4, 7, 2.95, 1),
+(5, 6, 6.50, 1),
+(5, 4, 6.00, 1),
+(6, 1, 4.50, 10);
